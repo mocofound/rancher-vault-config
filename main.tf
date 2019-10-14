96a5d3307e007a299e21257f1f65deada2b2db3c
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-10-14 13:08:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "vault" {
 }
 
 data "terraform_remote_state" "userdata_server" {
-  backend = "remote"
+  backend = "atlas"
   config = {
     name = "${var.tfe_organization}/${var.k8s_cluster_workspace}"
   }
